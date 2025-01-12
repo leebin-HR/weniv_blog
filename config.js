6ffeafbf728bab37c228c8fbc4a989fb66d56fe9
leebin-HR <bunny9500@naver.com>
2025-01-12 21:02:18 +0900

    프로필 변경

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
+    username: "Leebin",
     company: "weniv",
     position: "CEO",
-    img: "img/user/profile-licat.png",
+    img: "img/user/a.JPG",
   },
 ];
 
